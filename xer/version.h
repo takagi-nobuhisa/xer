5a4474730cb069d11a62004f6d1e33f8b6eb7b39
takagi-nobuhisa <takagi.nobuhisa@gmail.com>
2026-04-11 12:54:42 +0900

    [add] xer/version.h

---
+++ xer/version.h
@@ -0,0 +1,22 @@
+﻿#ifndef XER_VERSION_H
+#define XER_VERSION_H
+
+#include <string_view>
+
+#define XER_VERSION_MAJOR 0
+#define XER_VERSION_MINOR 1
+#define XER_VERSION_PATCH 0
+#define XER_VERSION_SUFFIX "a1"
+#define XER_VERSION_STRING "0.1.0a1"
+
+namespace xer {
+
+inline constexpr int version_major = XER_VERSION_MAJOR;
+inline constexpr int version_minor = XER_VERSION_MINOR;
+inline constexpr int version_patch = XER_VERSION_PATCH;
+inline constexpr std::string_view version_suffix = XER_VERSION_SUFFIX;
+inline constexpr std::string_view version_string = XER_VERSION_STRING;
+
+} // namespace xer
+
+#endif
