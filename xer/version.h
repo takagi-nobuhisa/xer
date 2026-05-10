8c6dd42b0044b712de12a5dc6ab3861da4d153e0
takagi-nobuhisa <takagi.nobuhisa@gmail.com>
2026-05-10 12:43:58 +0900

    [update] バージョン情報を0.3.0a5に更新

--- xer/version.h
+++ xer/version.h
@@ -28,14 +28,14 @@
  *
  * This macro holds the non-numeric suffix part of the version string.
  */
-#define XER_VERSION_SUFFIX "a4"
+#define XER_VERSION_SUFFIX "a5"
 
 /**
  * @brief Full version string of XER.
  *
  * This macro holds the complete version string.
  */
-#define XER_VERSION_STRING "0.3.0a4"
+#define XER_VERSION_STRING "0.3.0a5"
 
 namespace xer {
 
